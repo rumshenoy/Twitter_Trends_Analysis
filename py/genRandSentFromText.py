d7e00f1ae03cec3322cbe94cb6c2cbfccccde0ae
Ramya Shenoy <rumshenoy@gmail.com>
2014-01-28 18:51:34 -0800

    Revert "Adding gitignore"
    This reverts commit d46752337c60c401fd9bc37e020dd7d92b142f85.

--- py/genRandSentFromText.py
+++ py/genRandSentFromText.py
@@ -4,19 +4,10 @@ import sys
 import nltk
 
 def generateRandom(filename):
-    #file_ = open("jeeves.txt")
-    #file_ = codecs.open("firefox.txt", "r", "utf-8")
     file_ = open(filename)
-    #markov = markovgenpy.Markov(file_)
-    #sentence = markov.generate_markov_text()
-    #file_ = codecs.open(filename,'rb','utf-8')
-    #file_ = open("firefox.txt")
     markov = markovgenpy.Markov(file_)
     sentence = markov.generate_markov_text()
     return sentence
-
-
-
 
 if len(sys.argv) < 2:
     print "Please specify a number as input."
